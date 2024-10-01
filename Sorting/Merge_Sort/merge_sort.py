6e72b0f06e8d0b57433ac58c4016154c1b4126c2
jskrajareddy21 <kriss044@gmail.com>
2024-10-01 07:06:21 +0530

    merge sort algorithm

---
+++ Sorting/Merge_Sort/merge_sort.py
@@ -0,0 +1,37 @@
+def merge_sort(arr):
+    if len(arr) > 1:
+        left_arr = arr[:len(arr)//2]
+        right_arr = arr[len(arr)//2:]
+
+        #recursion
+        merge_sort(left_arr)
+        merge_sort(right_arr)
+
+        #merge
+        i,j,k=0,0,0
+
+        while i < len(left_arr) and j < len(right_arr):
+            if left_arr[i] < right_arr[j]:
+                arr[k] = left_arr[i]
+                i += 1
+            else:
+                arr[k] = right_arr[j]
+                j += 1
+
+            k += 1
+
+        while i < len(left_arr):
+            arr[k] = left_arr[i]
+            i += 1
+            k += 1
+
+        while j < len(right_arr):
+            arr[k] = right_arr[j]
+            j += 1
+            k += 1
+
+    return arr
+
+unsorted_list = [ 7, 12, 9, 11, 3]
+result = merge_sort(unsorted_list)
+print("Merge sort array : ",result)
