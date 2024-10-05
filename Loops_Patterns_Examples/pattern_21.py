4dda82b0cec074f0baeb21e7f1e7d31261d9a6b3
jskrajareddy21 <kriss044@gmail.com>
2024-10-05 20:45:55 +0530

    loop pattern examples

---
+++ Loops_Patterns_Examples/pattern_21.py
@@ -0,0 +1,15 @@
+# Output
+#          1
+#         22
+#        333
+#       4444
+#      55555
+#     666666
+#    7777777
+#   88888888
+#  999999999
+# 10101010101010101010
+
+n = int(input("Enter the number of rows : "))
+for i in range(1, n+1):
+    print(' '*(n-i)+str(i)*i+' '*(n-i))
